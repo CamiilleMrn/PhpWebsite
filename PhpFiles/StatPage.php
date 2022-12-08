b21c99335f8d83c8b34ee54e403863a79815a3ab
CamiilleMrn <marion_camille@yahoo.com>
2022-12-08 20:58:07 +0100

    cleanup

--- PhpFiles/StatPage.php
+++ PhpFiles/StatPage.php
@@ -7,16 +7,16 @@
         <head>
             <title> Liste des joueurs </title>
             <meta charset="UTF-8">
-            <link rel="stylesheet" href="PlayerList.css">
+            <link rel="stylesheet" href="../CSS/PlayerList.css">
         </head>
 
         <body>
             <nav class = "menu">
-                <label class = "logo"><img src="ProjetPhpPhoto/LogoSite.jpg" alt="Logo du site"/></label>
+                <label class = "logo"><img src="../ProjetPhpPhoto/LogoSite.jpg" alt="Logo du site"/></label>
                 <ul>
-                    <li> <a class ="active" href="PlayerList.php"> Liste des joueurs</a> </li>
+                    <li> <a href="PlayerList.php"> Liste des joueurs</a> </li>
                     <li> <a href="MatchList.php"> Liste des matchs</a> </li>
-                    <li> <a href="StatPage.php"> Statistiques des joueurs</a> </li>
+                    <li> <a class ="active" href="StatPage.php"> Statistiques des joueurs</a> </li>
                 </ul>
             </nav>
         </body>
