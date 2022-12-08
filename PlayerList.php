914ef24639b5bd2db978fb190cc0257f91cd625e
CamiilleMrn <marion_camille@yahoo.com>
2022-12-08 19:27:56 +0100

    nav bar done

---
+++ PlayerList.php
@@ -0,0 +1,24 @@
+<?php
+
+?>
+
+<!DOCTYPE HTML>
+    <html>
+        <header>
+            <meta charset="UTF-8">
+            <title> Liste des joueurs </title>
+            <link rel="stylesheet" href="PlayerList.css">
+        </header>
+
+        <body>
+            <nav class = "menu">
+                <label class = "logo" href="#"><img src="ProjetPhpPhoto/LogoSite.jpg" alt="Logo du site"/></label>
+                <ul>
+                    <li> <a class ="active" href="PlayerList.php"> Liste des joueurs</a> </li>
+                    <li> <a href="MatchList.php"> Liste des matchs</a> </li>
+                    <li> <a href="StatPage.php"> Statistiques des joueurs</a> </li>
+                </ul>
+            </nav>
+        </body>
+
+    </html>
